3321b4b21134f1f02361e04b0a1e73a5b93f38d1
chocoby <chocoby@gmail.com>
2013-04-12 00:58:51 +0900

    Check content-length and content-type

--- app.rb
+++ app.rb
@@ -3,29 +3,42 @@ require 'faraday'
 require 'base64'
 require 'json'
 
+MAX_CONTENT_LENGTH = 100000
+ALLOW_CONTENT_TYPES = ['image/jpeg', 'image/png', 'image/gif']
+
 get '/' do
   'chatwork-allow-origin-proxy'
 end
 
 get '/v1/image.json' do
-  # TODO: content type (20KB)
-  # TODO: content length (jpeg/gif/png)
-
   return not_found unless params[:url]
 
   content_type :json
 
   conn = Faraday::Connection.new
-  response = conn.get params[:url]
+  response = conn.head params[:url]
 
-  unless response.status == 200
-    status response.status
-    return { status: "status code #{response.status}" }.to_json
+  status_code = response.status
+  unless status_code == 200
+    status status_code
+    return { status: "status code #{status_code}" }.to_json
+  end
+
+  content_length = response.headers['content-length'].to_i
+  unless MAX_CONTENT_LENGTH >= content_length
+    status 500
+    return { status: "request icon is too large. max: 100KB" }.to_json
   end
 
   content_type = response.headers['content-type']
-  body = response.body
+  unless ALLOW_CONTENT_TYPES.include? content_type
+    status 500
+    return { status: "invalid content-type. allow: image/jpeg, image/png, image/gif" }.to_json
+  end
 
+  response = conn.get params[:url]
+
+  body = response.body
   encoded_body = Base64.encode64 body
 
   data_url = "data:#{content_type};base64,#{encoded_body}"
